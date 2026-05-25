c7f2aae75dffc9c00c4ea2c6ff8adac9e5713c1f
ThePraveen69 <praveenkumaryerrapilli69@gmail.com>
2026-05-25 11:07:20 +0530

    Add files via upload

---
+++ automation_beg.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import requests
+from io import StringIO
+
+url = "https://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes_(seasons_1%E2%80%9320)"
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+response = requests.get(url , headers=headers) 
+simpsons = pd.read_html(StringIO(response.text))
+print(len(simpsons))
